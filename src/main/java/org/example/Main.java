84a91d2526798486e92b56258bc64052791017db
Anastasija <atemelkonska99@hotmail.com>
2026-02-08 12:44:36 +0100

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Devops homework2!");
+        System.out.println("Devops homework3!");
     }
 }
---
+++ src/test/java/HomeworkTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeworkTest {
+
+    @Test
+    void testOk() {
+        assertTrue(true);
+    }
+}
