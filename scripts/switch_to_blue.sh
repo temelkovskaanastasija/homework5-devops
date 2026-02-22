4c42c02ed1b08fc3bf7390c4ebf4548f9bd6488e
Anastasija <atemelkonska99@hotmail.com>
2026-02-22 21:07:28 +0100

    Fix blue-green deploy in Jenkins

--- scripts/switch_to_blue.sh
+++ scripts/switch_to_blue.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-docker cp ./nginx/upstream_blue.conf bg-nginx:/etc/nginx/conf.d/active_upstream.conf
+docker exec bg-nginx sh -c 'cat > /etc/nginx/conf.d/active_upstream.conf <<EOF
+upstream active_backend {
+    server app-blue:8080;
+}
+EOF'
+
 docker exec bg-nginx nginx -s reload
 
 echo "Switched traffic to BLUE"
--- scripts/switch_to_green.sh
+++ scripts/switch_to_green.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-docker cp ./nginx/upstream_green.conf bg-nginx:/etc/nginx/conf.d/active_upstream.conf
+docker exec bg-nginx sh -c 'cat > /etc/nginx/conf.d/active_upstream.conf <<EOF
+upstream active_backend {
+    server app-green:8080;
+}
+EOF'
+
 docker exec bg-nginx nginx -s reload
 
 echo "Switched traffic to GREEN"
