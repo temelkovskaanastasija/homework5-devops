a93902c249cb8855ba14652ae051483236af3893
Anastasija <atemelkonska99@hotmail.com>
2026-02-22 20:45:58 +0100

    Add Blue-Green deployment pipeline test

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -22,6 +22,6 @@ public class IntegrationTest {
         String body = in.readLine();
         in.close();
 
-        assertEquals("OK", body);
+        assertEquals("{\"status\":true}", body);
     }
 }
