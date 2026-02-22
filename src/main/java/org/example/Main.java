882a93de5b330970ed20ac54ad2bf125d7bea16e
Anastasija <atemelkonska99@hotmail.com>
2026-02-22 21:12:24 +0100

    Fix blue-green deploy in Jenkins

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,7 +18,7 @@ public class Main {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
         server.createContext("/", exchange -> {
-            String response = "DevOps Homework4 - " + env + " version=" + version;
+            String response = "DevOps Homework4 test - " + env + " version=" + version;
             sendResponse(exchange, response);
         });
 
